9646ec3d2ba037c5b17fb482a9e1288c29ba2a33
David <ing.dfgarayp@gmail.com>
2021-03-17 16:29:43 -0500

    basic components

---
+++ src/components/header/header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class header extends Component {
+  // constructor(props){
+    // super(props);
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default header;
